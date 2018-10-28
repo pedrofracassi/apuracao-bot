da382f08bd10db411beb7d0be64fe25696059153
Eduardo Londero <eduardolonderovieira@gmail.com>
2018-10-28 16:10:02 -0300

    UF toLowerCase()
    BECAUSE IT'S FUCKING FRUSTRATING TO TYPE RS AND IT RETURNS ERROR

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js')
-const snekfetch = require('snekfetch')
-const client = new Discord.Client()
-const RichEmbed = Discord.RichEmbed
+const Discord = require('discord.js');
+const snekfetch = require('snekfetch');
+const client = new Discord.Client();
+const { RichEmbed } = Discord;
 
 client.on('ready', () => {
-  console.log(`Logado como ${client.user.tag}!`)
-})
+  console.log(`Logado como ${client.user.tag}!`);
+});
 
 client.on('message', async msg => {
   if (msg.content.startsWith(`<@${client.user.id}>`)) {
@@ -13,7 +13,7 @@ client.on('message', async msg => {
     msg.channel.startTyping()
     const where = msg.content.split(' ')[1] || 'br'
     try {
-      const { body } = await snekfetch.get(`https://s.glbimg.com/jo/el/2018/apuracao/2-turno/${where}/executivo.json`)
+      const { body } = await snekfetch.get(`https://s.glbimg.com/jo/el/2018/apuracao/2-turno/${where.toLowerCase()}/executivo.json`);
 
       const info = new RichEmbed()
         .setTitle(`${body.abrangencia.nome} (${body.abrangencia.andamento}%)`)
@@ -26,29 +26,29 @@ client.on('message', async msg => {
           ` ${process.env.BOT_TAG_EMOJI} [Adicione o **Apuração Bot** ao seu servidor](https://discordapp.com/oauth2/authorize?client_id=506142008415485963&scope=bot)`,
           ` ${process.env.GITHUB_EMOJI} [Veja o código-fonte no GitHub](https://github.com/pedrofracassi/apuracao-bot)`
         ].join('\n'))
-        .setColor(0x7289da)
-      await msg.channel.send(info)
+        .setColor(0x7289da);
+      
+      await msg.channel.send(info);
 
       body.candidatos.forEach(async candidato => {
         const embed = new RichEmbed()
           .setAuthor(`${candidato.nome} (${candidato.partido})`, candidato.foto, candidato.politicoUrl)
-          .setDescription(`**${candidato.votos.quantidade} votos** (${candidato.votos.porcentagem}%)`)
-        await msg.channel.send(embed)
-      })
-
+          .setDescription(`**${candidato.votos.quantidade} votos** (${candidato.votos.porcentagem}%)`);
+        
+        await msg.channel.send(embed);
+      });
     } catch (e) {
+      console.error(e);
       if (e.message === '404 Not Found') {
-        msg.channel.send(new RichEmbed().setColor(0xF05958).setDescription('**UF não encontrada.** Verifique se você digitou corretamente.'))
-        console.log(e)
+        msg.channel.send(new RichEmbed().setColor(0xF05958).setDescription('**UF não encontrada.** Verifique se você digitou corretamente.'));
       } else {
         msg.channel.send(new RichEmbed().setColor(0xF05958).setDescription([
           '**Um erro ocorreu.** [Clique aqui para reportar ao desenvolvedor.](https://github.com/pedrofracassi/apuracao-bot/issues/new)',
           '\`' + e.message +'\`'
-        ].join('\n')))
-        console.log(e)
+        ].join('\n')));
       }
     }
-    msg.channel.stopTyping()
+    msg.channel.stopTyping();
   }
 })
 
